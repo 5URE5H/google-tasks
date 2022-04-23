c2b91690ca47dc7df163946de1fd8cd8d88b7724
Suresh Reddy <sureshreddy3105@gmail.com>
2022-04-23 13:00:25 +0530

    Ability to rename and delete tasklist

--- src/components/tasklist/TaskListMenu.tsx
+++ src/components/tasklist/TaskListMenu.tsx
@@ -9,10 +9,19 @@ import Divider from "@mui/material/Divider";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import { Check } from "@mui/icons-material";
 import Paper from "@mui/material/Paper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { deleteTaskListApi, updateTaskListApi } from "../../api";
+import { useTaskList } from "../../store/TaskList.store";
+import {
+  DELETE_TASKLIST,
+  SELECT_TASKLIST,
+  UPDATE_TASKLIST,
+} from "../../store/constants";
+import Button from "@mui/material/Button";
+import Input from "@mui/material/Input";
 
 const style = {
   position: "absolute" as "absolute",
@@ -21,18 +30,32 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  borderRadius: 2,
+  p: 3,
 };
 
 export default function TaskListMenu() {
+  const [tasklistState, tasklistDispatch] = useTaskList();
+
+  const [taskListTitle, setTaskListTitle] = useState(
+    tasklistState.selected?.title
+  );
+
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const handleOpenDeleteModal = () => setOpenDeleteModal(true);
+  const handleCloseDeleteModal = () => setOpenDeleteModal(false);
 
   const [openModal, setOpenModal] = useState(false);
   const handleOpenModal = () => setOpenModal(true);
   const handleCloseModal = () => setOpenModal(false);
+
+  useEffect(() => {
+    setTaskListTitle(tasklistState.selected?.title);
+  }, [tasklistState.selected?.title]);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -40,6 +63,40 @@ export default function TaskListMenu() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleDeleteList = () => {
+    deleteTaskListApi({ taskListId: tasklistState.selected?.id }).then(() => {
+      tasklistDispatch({
+        type: SELECT_TASKLIST,
+        payload: tasklistState.items[0],
+      });
+      tasklistDispatch({
+        type: DELETE_TASKLIST,
+        payload: tasklistState.selected,
+      });
+    });
+    handleCloseDeleteModal();
+  };
+
+  const handleTasklistChange = (title: string) => {
+    setTaskListTitle(title);
+  };
+
+  const handleTasklistDone = () => {
+    updateTaskListApi({
+      taskListId: tasklistState.selected?.id,
+      title: taskListTitle,
+    }).then((response) => {
+      tasklistDispatch({ type: UPDATE_TASKLIST, payload: response });
+      tasklistDispatch({ type: SELECT_TASKLIST, payload: response });
+    });
+    handleCloseModal();
+  };
+
+  const handleTasklistCancel = () => {
+    setTaskListTitle(tasklistState.selected?.title);
+    handleCloseModal();
   };
 
   return (
@@ -78,7 +135,9 @@ export default function TaskListMenu() {
             <ListItemText onClick={handleOpenModal}>Rename list</ListItemText>
           </MenuItem>
           <MenuItem>
-            <ListItemText>Delete list</ListItemText>
+            <ListItemText onClick={handleOpenDeleteModal}>
+              Delete list
+            </ListItemText>
           </MenuItem>
           <MenuItem>
             <ListItemText>Delete all completed tasks</ListItemText>
@@ -97,12 +156,52 @@ export default function TaskListMenu() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6">
-            Text in a modal
+          <Typography variant="h6">Rename list</Typography>
+          <Input
+            fullWidth
+            id="standard-multiline-static"
+            value={taskListTitle}
+            onChange={(e) => handleTasklistChange(e.target.value)}
+            placeholder="Enter name"
+          />
+          <div className="custom-modal-buttons-container">
+            <Button
+              size="medium"
+              color="secondary"
+              onClick={handleTasklistCancel}
+            >
+              Cancel
+            </Button>
+            <Button size="medium" onClick={handleTasklistDone}>
+              Done
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+
+      <Modal
+        open={openDeleteModal}
+        onClose={handleCloseDeleteModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography variant="h6">Delete this list?</Typography>
+          <Typography variant="subtitle1" sx={{ mt: 2 }}>
+            All tasks in this list will be permanently deleted
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className="custom-modal-buttons-container">
+            <Button
+              size="medium"
+              color="secondary"
+              onClick={handleCloseDeleteModal}
+            >
+              Cancel
+            </Button>
+            <Button size="medium" onClick={handleDeleteList}>
+              Delete
+            </Button>
+          </div>
         </Box>
       </Modal>
     </div>
